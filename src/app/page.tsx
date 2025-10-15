0f89c3ca7e38ce12dc5a402f90c48aeba4e021f3
Shariar Hasan Emon <imemon.sh@gmail.com>
2025-10-15 22:25:18 +0600

    feat: auth kit finalize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <section className={"container space-y-4 sm:space-y-6 lg:space-y-10"}>
       <div className="min-h-[calc(100vh-3rem)] sm:min-h-[calc(100vh-3.5rem)] md:min-h-[calc(100vh-4.5rem)] flex flex-col items-center justify-center">
-        <h1 className="text-xl flex items-center">
+        <h1 className="text-base font-semibold flex items-center uppercase">
           Nextjs Laravel auth kit by
           <Button variant={"link"}>
             <ArrowUpRightIcon />
